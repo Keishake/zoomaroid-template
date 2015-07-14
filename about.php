3aae3fec3481a06c33c94d61c11732940a2e311a
Keishake <keishake0925@gmail.com>
2015-07-14 23:31:00 +0900

    add about page

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: About
+*/
+?>
+<?php get_header(); ?>
+<main id="main" class="mdl-layout__content">
+  <div class="blog-content">
+    <section class="blog-about mdl-grid">
+      <a href="https://itunes.apple.com/us/app/endoresu-tian-he/id581871752?mt=8" target="blank">
+      <div class="mdl-card mdl-cell mdl-cell--4-col mdl-shadow--4dp blog-card-image image-tenho">
+        <div class="mdl-card__title mdl-card--expand"></div>
+        <div class="mdl-card__actions">
+          <span class="blog-card-image__filename">エンドレス天和</span>
+        </div>
+      </div>
+      </a>
+      <a href="https://itunes.apple.com/us/app/naotore!-zhi-dingjanken/id584792963?mt=8" target="blank">
+      <div class="mdl-card mdl-cell mdl-cell--4-col mdl-shadow--4dp blog-card-image image-janken">
+        <div class="mdl-card__title mdl-card--expand"></div>
+        <div class="mdl-card__actions">
+          <span class="blog-card-image__filename">脳トレ！指定じゃんけん</span>
+        </div>
+      </div>
+      </a>
+    </section>
+  <?php get_footer(); ?>
+</main>
